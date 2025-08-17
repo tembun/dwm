d47917c5d071a14a963bc24d3c7fe20751e43f1f
Artem Bunichev <tembun@bk.ru>
2025-08-17 20:53:34 +0300

    config: Reduce font size 12 -> 8
    The only place the font size is used in my version
    is bar.

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int flbordpx	= 2;	/* border pixel for floating */
 static const unsigned int snap		= 32;	/* snap pixel */
 static const int showbar		= 0;	/* 0 means no bar */
 static const int topbar			= 1;	/* 0 means bottom bar */
-static const char *fonts[]          = { "terminus:size=12" };
+static const char *fonts[]          = { "terminus:size=8" };
 static const char dmenufont[]       = "terminus:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
