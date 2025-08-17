f4f24f7c60adb5074046b7dbb6bd5c6d43810691
Artem Bunichev <tembun@bk.ru>
2025-08-17 15:34:21 +0300

    config: Space/tab fixes

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx	= 3;	/* border pixel of windows */
 static const unsigned int flbordpx	= 2;	/* border pixel for floating */
-static const unsigned int snap 		= 32;	/* snap pixel */
+static const unsigned int snap		= 32;	/* snap pixel */
 static const int showbar		= 0;	/* 0 means no bar */
 static const int topbar			= 1;	/* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=12" };
