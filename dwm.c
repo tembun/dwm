8633144228688876870e0493cda91855a293b0d2
Artem Bunichev <tembun@bk.ru>
2025-08-19 14:56:07 +0300

    dwm: Always use normal color scheme for bar

--- dwm.c
+++ dwm.c
@@ -691,12 +691,9 @@ drawbar(Monitor *m)
 		return;
 
 	if ((w = m->ww - tw - x) > bh) {
-		if (m->sel) {
-			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
+		drw_setscheme(drw, scheme[SchemeNorm]);
+		if (m->sel)
 			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
-		} else {
-			drw_setscheme(drw, scheme[SchemeNorm]);
-		}
 	}
 	drw_map(drw, m->barwin, 0, 0, m->ww, bh);
 }
